0ba9d565d255c3ebf00423c6a0959e2058cd829a
NiciusB <nuno@balbona.me>
2021-04-03 22:59:48 +0200

    Improve graphql DX

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,4 +13,7 @@ module.exports = {
 	rules: {
 		'prettier/prettier': 'error',
 	},
+	globals: {
+		ApiTypes: 'readonly',
+	},
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import dotenv from 'dotenv'
-import api from './castle-api'
+import { GraphQLClient } from 'graphql-request'
+import { getSdk } from './castle-api/generated/api'
 dotenv.config()
+
+const client = new GraphQLClient('https://api.castle.games/graphql')
+const api = getSdk(client)
 
 async function main() {
 	if (!process.env.CASTLE_USERNAME || !process.env.CASTLE_PASSWORD) {
@@ -9,10 +13,23 @@ async function main() {
 		)
 	}
 
-	const token = await api.login(
-		process.env.CASTLE_USERNAME,
-		process.env.CASTLE_PASSWORD
-	)
-	console.log(token)
+	const userId = await api
+		.userForLoginInput({ who: process.env.CASTLE_USERNAME })
+		.then((r) => r.userForLoginInput?.userId)
+	const token = await api
+		.login({
+			userId,
+			password: process.env.CASTLE_PASSWORD,
+		})
+		.then((r) => r.login?.token)
+
+	if (!token) {
+		throw new Error('Token not found')
+	}
+
+	client.setHeader('X-Auth-Token', token)
+
+	const decks = await api.getMyDecks()
+	console.log(JSON.stringify(decks, null, 2))
 }
 main()
